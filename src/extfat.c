85eba14faeaa51620a129483e20bff5204a0b634
uta-sgs7099 <123352230+uta-sgs7099@users.noreply.github.com>
2023-03-03 06:36:00 +0000

    message

--- src/extfat.c
+++ src/extfat.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stddef.h>
+#include <ctype.h>
+#include <getopt.h>//add this
+#include <string.h>//a dd this
+#include "bootSec.h"
 #include "../include/fileInfo.h"
 #include "../include/fileManip.h"
 
