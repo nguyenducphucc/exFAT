f950b753b3c726e34dfad674722c720ad2c005f5
princesspenguin <123342243+princesspenguin@users.noreply.github.com>
2023-03-29 23:34:30 +0000

    blah

---
+++ include/bootSec.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int cmp_struct(Main_Boot* MB, Main_Boot* BB);
+void bootCheck(Option op);
--- src/extfat.c
+++ src/extfat.c
@@ -1,15 +1,15 @@
 #include "fileInfo.c"
 #include "verify.c"
-//#include "fileManip.c"
-//#include "bootSec.c"
+#include "fileManip.c"
+#include "bootSec.c"
 
 int main(int argc, char *argv[]) {
     Option op = inputProcess(argc, argv);
     if(op.helpFlag || op.errorFlag) return 0;
 
 
-    //copyInputFileToAnotherFile(op);
-    //bootCheck(op);
+    copyInputFileToAnotherFile(op);
+    bootCheck(op);
 
     return 0;
 }
