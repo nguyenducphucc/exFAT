816974f1db9a6eabc4ae846ed2680c01d9103cd0
princesspenguin <123342243+princesspenguin@users.noreply.github.com>
2023-04-02 07:47:08 +0000

    m

--- include/verfiy.c
+++ include/verfiy.c
@@ -1,3 +1,15 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stddef.h>
+#include <ctype.h>
+
 #include "verify.h"
 #include "routine.h"
 
--- include/verify.h
+++ include/verify.h
@@ -1,3 +1,3 @@
 #pragma once
 
-void checkSum(Option op)
+void checkSum(Option op);
